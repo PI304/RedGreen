358ff19261efa41c0a5be61662f3dfe765bd640e
linda2927 <linda2927@naver.com>
2023-02-21 15:48:34 +0900

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.get('/status', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('join', (room) => {
+      socket.join(room);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg);
+
+        socket.to('demo-room').emit('status', msg);
+      });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
